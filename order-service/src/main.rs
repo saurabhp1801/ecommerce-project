119dbdf11b408f8f6188efd952550842a9ae411c
Saurabh Pandey <saurabhp643@gmail.com>
2025-06-12 19:46:17 +0530

    order services update1

--- order-service/src/main.rs
+++ order-service/src/main.rs
@@ -2,6 +2,8 @@ use axum::{serve};
 use std::net::SocketAddr;
 use sqlx::postgres::PgPoolOptions;
 use tokio::net::TcpListener;
+use tracing::{info, error};
+use tracing_subscriber;
 
 mod handlers;
 mod routes;
@@ -10,15 +12,21 @@ mod errors;
 
 #[tokio::main]
 async fn main() {
+    // Initialize logger
+    tracing_subscriber::fmt::init();
+
     let db_pool = PgPoolOptions::new()
         .connect("postgres://postgres:password@localhost:5432/orders_db")
         .await
-        .expect("DB connection failed");
+        .unwrap_or_else(|e| {
+            error!("DB connection failed: {}", e);
+            std::process::exit(1);
+        });
 
     let app = routes::order_routes(db_pool.clone());
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 3001));
-    println!("Listening on http://{}", addr);
+    info!("Listening on http://{}", addr);
 
     let listener = TcpListener::bind(&addr).await.unwrap();
     serve(listener, app).await.unwrap();
